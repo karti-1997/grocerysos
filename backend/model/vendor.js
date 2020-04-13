5b40efc425329462d8542612368d776f3447dcca
dhanumdp <43883013+dhanumdp@users.noreply.github.com>
2020-04-13 22:31:33 +0530

    Update vendor.js

--- backend/model/vendor.js
+++ backend/model/vendor.js
@@ -1,10 +1,81 @@
-const mongoose = require('mongoose');
 
-const VendorSchema = mongoose.Schema({
-
-    
-});
+var mongoose = require('mongoose');
 
 
-module.exports = mongoose.model('Vendor',vendorSchema);
+const Vendor = mongoose.model('Vendor',{
 
+    vendorId: {
+        type: String, // autoGen by Us.
+        required: true
+    },
+    storeName : {
+        type: String,
+        required: true
+    },
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String
+    },
+    address: [{
+        careOf: {
+            type: String
+        },
+        doorNo: {
+            type: String,
+            required: true
+        },
+        addressLine1: {
+            type: String,
+            required: true
+        },
+        addressLine2: {
+            type: String
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        district: {
+            type: String,
+            required:true
+        },
+        pinCode: {
+            type: String,
+            required: true
+        },
+        country: {
+            type: String,
+            required: true
+        }
+    }],
+    contact: [{
+        whatsApp: {
+            type: Number,
+            required: true
+        },
+        mobile: {
+            type: Number
+        }
+    }],
+    createdBy: {
+        type: String, // franchiseAdmin name who creates it or vendor name
+        required: true
+    },
+    deliveryWindow: {
+        type: String,
+        required: true
+    },
+    pickupWindow: {
+        type: String,
+        required: true    
+    }
+
+},'Vendors')
+
+
+
+
+module.exports = { Vendor };
